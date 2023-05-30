c6ff86397bbec2d0b9aba6d8e401a31acf76ba36
Martin Goulet <martin.goulet@live.ca>
2023-05-30 08:27:03 -0400

    Add my name to files

--- wizardsgrimoire.action.php
+++ wizardsgrimoire.action.php
@@ -3,7 +3,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * WizardsGrimoire implementation : © <Your name here> <Your email address here>
+ * WizardsGrimoire implementation : © Martin Goulet <martin.goulet@live.ca>
  *
  * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
  * See http://en.doc.boardgamearena.com/Studio for more information.
