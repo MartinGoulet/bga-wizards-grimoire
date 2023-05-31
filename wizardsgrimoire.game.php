f309bef219e2e6b3a0327dffe5916fac94155f4b
Martin Goulet <martin.goulet@live.ca>
2023-05-31 06:13:56 -0400

    Starting point at 60

--- wizardsgrimoire.game.php
+++ wizardsgrimoire.game.php
@@ -19,7 +19,7 @@
 
 $swdNamespaceAutoload = function ($class) {
     $classParts = explode('\\', $class);
-    if ($classParts[0] == 'DiceThrone') {
+    if ($classParts[0] == 'WizardsGrimoie') {
         array_shift($classParts);
         $file = dirname(__FILE__) . '/modules/php/' . implode(DIRECTORY_SEPARATOR, $classParts) . '.php';
         if (file_exists($file)) {
@@ -101,11 +101,11 @@ class WizardsGrimoire extends Table {
 
         // Create players
         // Note: if you added some extra field on "player" table in the database (dbmodel.sql), you can initialize it there.
-        $sql = "INSERT INTO player (player_id, player_color, player_canal, player_name, player_avatar) VALUES ";
+        $sql = "INSERT INTO player (player_id, player_color, player_canal, player_name, player_avatar, player_score) VALUES ";
         $values = array();
         foreach ($players as $player_id => $player) {
             $color = array_shift($default_colors);
-            $values[] = "('" . $player_id . "','$color','" . $player['player_canal'] . "','" . addslashes($player['player_name']) . "','" . addslashes($player['player_avatar']) . "')";
+            $values[] = "('" . $player_id . "','$color','" . $player['player_canal'] . "','" . addslashes($player['player_name']) . "','" . addslashes($player['player_avatar']) . "', 60)";
         }
         $sql .= implode(',', $values);
         self::DbQuery($sql);
