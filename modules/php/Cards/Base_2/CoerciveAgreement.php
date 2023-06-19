cbf497547cf5818d31513c78f5bff59da436293b
Martin Goulet <martin.goulet@live.ca>
2023-06-19 13:05:01 -0400

    Temp

--- modules/php/Cards/Base_2/CoerciveAgreement.php
+++ modules/php/Cards/Base_2/CoerciveAgreement.php
@@ -4,10 +4,32 @@ namespace WizardsGrimoire\Cards\Base_2;
 
 use WizardsGrimoire\Cards\BaseCard;
 use WizardsGrimoire\Core\Game;
+use WizardsGrimoire\Core\ManaCard;
+use WizardsGrimoire\Core\Players;
 use WizardsGrimoire\Objects\CardLocation;
 
 class CoerciveAgreement extends BaseCard {
 
-    // Choose 1: Take up to 3 randomly selected mana from your opponent's hand, or discard a mana card off 2 of your other spells.
+    
+    public function castSpell($args) {
+        // Choose 1: Take up to 3 randomly selected mana from your opponent's hand, or discard a mana card off 2 of your other spells.
+        if ($args == null || $args == "") {
+            $opponent_id = Players::getOpponentId();
+            $cards = Players::getHand($opponent_id)
+
+            if(sizeof($cards) <= 3) {
+                $ids = array_map(function($card) { return $card['id'] }, $cards);
+                Game::get()->deck_manas->moveCards($ids, CardLocation::Hand(), Players::getPlayerId());
+                $cards_after = ManaCard::getCards($ids);
+            } else {
+                
+            }
+        } else {
+            $position = intval(array_shift($args));
+            ManaCard::discardManaFromSpell($position);
+            $position = intval(array_shift($args));
+            ManaCard::discardManaFromSpell($position);
+        }
+    }
 
 }
