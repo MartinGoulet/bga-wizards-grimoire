deb0bd57ac3edf5b1182fe0942416469de967681
Martin Goulet <martin.goulet@live.ca>
2023-07-09 15:07:01 -0400

    Trigger ongoing on onManaDiscarded

--- modules/php/Core/Events.php
+++ modules/php/Core/Events.php
@@ -25,17 +25,24 @@ class Events {
         }
         $spell = SpellCard::getFromRepertoire($position, $player_id);
         $card_type = SpellCard::getCardInfo($spell);
-        if ($card_type['activation'] == WG_SPELL_ACTIVATION_DELAYED) {
-            $instance = SpellCard::getInstanceOfCard($spell);
-            if ($instance->isDelayedSpellTrigger()) {
-                if ($card_type['activation_auto'] == true) {
-                    $instance->castSpell($mana_card);
-                } else {
-                    $card_ids = Globals::getCoolDownDelayedSpellIds();
-                    $card_ids[] = $spell['id'];
-                    Globals::setCoolDownDelayedSpellIds($card_ids);
+        switch ($card_type['activation']) {
+            case WG_SPELL_ACTIVATION_DELAYED:
+                $instance = SpellCard::getInstanceOfCard($spell);
+                if ($instance->isDelayedSpellTrigger()) {
+                    if ($card_type['activation_auto'] == true) {
+                        $instance->castSpell($mana_card);
+                    } else {
+                        $card_ids = Globals::getCoolDownDelayedSpellIds();
+                        $card_ids[] = $spell['id'];
+                        Globals::setCoolDownDelayedSpellIds($card_ids);
+                    }
                 }
-            }
+                break;
+            case WG_SPELL_ACTIVATION_ONGOING:
+                $instance = SpellCard::getInstanceOfCard($spell);
+                $count = ManaCard::countOnTopOfManaCoolDown($position, $player_id);
+                $instance->isOngoingSpellActive($count > 0, $player_id);
+                break;
         }
     }
 
