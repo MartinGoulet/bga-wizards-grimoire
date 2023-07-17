6d7a78acb75c3c3e27b36e38699c682e9ec8c730
Martin Goulet <martin.goulet@live.ca>
2023-07-16 21:06:22 -0400

    Suggestion 93523 - Skip discard of Possessed when opponent has an empty hand

--- modules/php/Cards/Base_2/Possessed.php
+++ modules/php/Cards/Base_2/Possessed.php
@@ -4,6 +4,7 @@ namespace WizardsGrimoire\Cards\Base_2;
 
 use WizardsGrimoire\Cards\BaseCard;
 use WizardsGrimoire\Core\Events;
+use WizardsGrimoire\Core\Globals;
 use WizardsGrimoire\Core\ManaCard;
 use WizardsGrimoire\Core\Notifications;
 use WizardsGrimoire\Core\Players;
@@ -12,6 +13,10 @@ class Possessed extends BaseCard {
 
     public function castSpell($args) {
         // Deal 5 damage. Your opponent may give you a mana from their hand. If they do, reduce your damage by the mana's power
+        if (ManaCard::getHandCount(Players::getOpponentId()) == 0) {
+            Globals::setSkipInteraction(true);
+            $this->castSpellInteraction(null);
+        }
     }
 
     public function castSpellInteraction($args) {
