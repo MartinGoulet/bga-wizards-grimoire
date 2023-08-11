a260422ecd14b385050d33c639e193fadd3819db
Martin Goulet <martin.goulet@live.ca>
2023-08-11 13:35:10 -0400

    95635: Cast fatal flaw then soul pact but didn’t work properly

--- modules/php/Cards/KickStarter_1/FatalFlaw.php
+++ modules/php/Cards/KickStarter_1/FatalFlaw.php
@@ -12,15 +12,14 @@ class FatalFlaw extends BaseCard {
 
     public function castSpell($args) {
         // Deal 1 damage. Reveal the top mana card on 1 of your opponents spells, dealing additional damage equal to it's power.
-        $this->dealDamage(1);
 
         if($args == null || $args == "") {
-            $this->dealDamage(0);
+            $this->dealDamage(1);
         } else {
             $position = intval(array_shift($args));
             $card = ManaCard::getOnTopOnManaCoolDown($position, Players::getOpponentId());
             Notifications::revealManaCardCooldown(Players::getOpponentId(), $card, $this->getCardName());
-            $this->dealDamage(ManaCard::getPower($card));
+            $this->dealDamage(ManaCard::getPower($card) + 1);
             Game::undoSavepoint();
         }
     }
