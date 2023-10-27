63fa053239f8c1ad1715d728862c7b872e9d64f0
Martin Goulet <martin.goulet@live.ca>
2023-10-27 13:50:30 -0400

    #102385: "interacts between Secret Oath, Twist of Fate, Quick Swap went partial buggy"

--- modules/php/Cards/KickStarter_1/QuickSwap.php
+++ modules/php/Cards/KickStarter_1/QuickSwap.php
@@ -3,7 +3,9 @@
 namespace WizardsGrimoire\Cards\KickStarter_1;
 
 use WizardsGrimoire\Cards\BaseCard;
+use WizardsGrimoire\Core\ManaCard;
 use WizardsGrimoire\Core\Notifications;
+use WizardsGrimoire\Core\Players;
 use WizardsGrimoire\Core\SpellCard;
 
 class QuickSwap extends BaseCard {
@@ -14,11 +16,25 @@ class QuickSwap extends BaseCard {
         if($args == null || $args == "") {
             $this->dealDamage(1);
         } else {
-            $new_spell_id = intval(array_shift($args));
             $old_spell = $this->getCard();
+            $old_spell_pos = SpellCard::getPositionInRepertoire($old_spell);
+
+            $new_spell_id = intval(array_shift($args));
             $new_spell = SpellCard::isInPool($new_spell_id);
             Notifications::discardSpellCard($this->getCardName());
             SpellCard::replaceSpell($old_spell, $new_spell);
+
+            if (ManaCard::countOnTopOfManaCoolDown($old_spell_pos) > 0) {
+                $this->checkOngoingSpell($new_spell, true);
+            }
+        }
+    }
+
+    private function checkOngoingSpell($spell, bool $is_active) {
+        $card_type = SpellCard::getCardInfo($spell);
+        if ($card_type['activation'] == WG_SPELL_ACTIVATION_ONGOING) {
+            $instance = SpellCard::getInstanceOfCard($spell);
+            $instance->isOngoingSpellActive($is_active, Players::getPlayerId());
         }
     }
 }
