a7d95166fe5423ead963d35bbf56f1ac6c80ca9d
Martin Goulet <martin.goulet@live.ca>
2025-10-31 08:48:06 -0400

    #190600: "Unable to pass the turn without a basic attack after taking Infinite Flame"

--- modules/php/Cards/Shifting_Sand_1/InfiniteFlame.php
+++ modules/php/Cards/Shifting_Sand_1/InfiniteFlame.php
@@ -2,12 +2,16 @@
 
 namespace WizardsGrimoireExt\Cards\Shifting_Sand_1;
 
-use WizardsGrimoireExt\Cards\BaseCard;
+use WizardsGrimoireExt\Cards\OngoingBaseCard;
 use WizardsGrimoireExt\Core\ManaCard;
 use WizardsGrimoireExt\Core\Players;
 use WizardsGrimoireExt\Core\SpellCard;
 
-class InfiniteFlame extends BaseCard {
+class InfiniteFlame extends OngoingBaseCard {
+
+    public function isActive(): bool {
+        return $this->isActiveAtLeastOneMana();
+    }
 
     public function onModifyBasicAttackDamage(int $damage): int {
         $spell = SpellCard::get($this->id);
